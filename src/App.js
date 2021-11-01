ff2445c3b9934e3dc0949dffdf1b91595481abe5
teradaian <teradaian@gmail.com>
2021-11-01 08:14:41 -0700

    add display imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
+
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider';
 
 function App() {
   const [display, setDisplay] = useState({
@@ -20,34 +25,51 @@ function App() {
     location: ''
   })
 
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setDisplay(formData)
+  }
+
+  useEffect(() => {
+    console.log(display)
+  }, [display])
+
   return (
     <div className="App">
       <Box>
         <Paper>
-          <form>
+          <form onSubmit={handleSubmit}>
             <TextField
               type="text"
               name="firstName"
               label="First Name"
               value={formData.firstName}
+              onChange={handleChange}
             />
             <TextField
               type="text"
               name="lastName"
               label="Last Name"
               value={formData.lastName}
+              onChange={handleChange}
             />
             <TextField
               type="text"
               name="email"
               label="Email"
               value={formData.email}
+              onChange={handleChange}
             />
             <TextField
               type="text"
               name="location"
               label="Location"
               value={formData.location}
+              onChange={handleChange}
             />
             <Button type="submit"> Submit </Button>
           </form>
