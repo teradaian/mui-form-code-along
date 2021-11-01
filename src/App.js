4df112ba0abc4acc42d9eaaf9f54d4a73621292e
teradaian <teradaian@gmail.com>
2021-11-01 09:40:32 -0700

    add usememo for theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
+import useDarkMode from './Components/useDarkMode'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 import Form from './Components/Form'
 import Display from './Components/Display'
 
 function App() {
+  const [ mode, toggleMode ] = useDarkMode()
+
+  const theme = useMemo(
+    () => createTheme({
+      palette: {
+        mode: mode,
+      }
+    }), [mode]
+  )
+  
   const [display, setDisplay] = useState({
     firstName: 'random',
     lastName: 'name',
@@ -19,10 +32,12 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Display display={display} />
-      <Form handleSubmit={handleSubmit} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Display display={display} />
+        <Form handleSubmit={handleSubmit} />
+      </div>
+    </ThemeProvider>
   );
 }
 
