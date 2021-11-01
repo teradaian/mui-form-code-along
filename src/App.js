af75eac1b5ff893065c4797b2563a8dd9bd07967
teradaian <teradaian@gmail.com>
2021-11-01 07:46:55 -0700

    display form set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [display, setDisplay] = useState({
+    firstName: 'random',
+    lastName: 'name',
+    email: 'randomname@gmail.com',
+    location: 'seattle'
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
