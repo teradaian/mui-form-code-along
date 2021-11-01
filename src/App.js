33964cc6f5eed049f9ad152bd247a1436c0afb82
teradaian <teradaian@gmail.com>
2021-11-01 07:55:19 -0700

    add form imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import { useState } from 'react'
 
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+
 function App() {
   const [display, setDisplay] = useState({
     firstName: 'random',
     lastName: 'name',
     email: 'randomname@gmail.com',
     location: 'seattle'
+  })
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    location: ''
   })
 
   return (
